5d669a183fb190375ee4fd06ef3943b746b5263a
Siddharth <sidkaduskar.com>
2023-01-28 13:16:17 +0530

    initial commit

---
+++ frontend_for_ocr/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+    <BasicExample/>
+    <div className="App">
+    </div>
+    </>
+  );
+}
+
+export default App;
